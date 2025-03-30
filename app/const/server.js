2d9be85b17fc57d0e3a8ec8a5b6438df69f06120
bafang <ningfangxiao@futurevista.work>
2025-03-30 11:00:26 +0800

    :bug: 交易 mcp

---
+++ app/const/server.js
@@ -0,0 +1,11 @@
+
+export const ChainRpc =  {
+    "1": 'https://ethereum.blockpi.network/v1/rpc/3b978f6c083c19b2aeed3b23a371cf81570908d7',
+    "56": 'https://bsc.blockpi.network/v1/rpc/a8d3b85c45f5964fe74312f4b687d051a6d4026e',
+    "8453": 'https://base.blockpi.network/v1/rpc/61275810d624ea65bb2956572ff728ea5a408b67'
+}
+export const chainIdToMoralis = {
+    "1": 'eth',
+    "56": 'bsc',
+    "8453": 'base',
+}
